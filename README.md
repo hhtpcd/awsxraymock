21e008ed9c4a9d4bc856498f74ad86bb4f1df145
Harry Hodge <harry@harryhodge.co.uk>
2024-10-19 22:04:36 +0100

    feat: adds mock server for AWS Xray API
    intended to be used to test retries and backpressure
    against Xray API.
    
    Signed-off-by: Harry Hodge <harry@harryhodge.co.uk>

---
+++ README.md
@@ -0,0 +1,46 @@
+# awsxraymock
+
+Mock the AWS X-Ray API for testing. Can toggle on/off API throttling.
+
+## Build
+
+```sh
+go build -o awsxraymockserver -ldflags="-s -w" -v .
+```
+
+## Run
+
+```sh
+./awsxraymockserver
+```
+
+## Usage
+
+Set the API to normal operation:
+
+```sh
+curl -X POST http://localhost:8080/SetOK
+```
+
+Set the API to throttle operation, all requests will return 429:
+
+```sh
+curl -X POST http://localhost:8080/SetThrottled
+```
+
+Otherwise post JSON segment documents at the API endpoint.
+
+```sh
+START_TIME=$(date +%s)
+HEX_TIME=$(printf '%x\n' $START_TIME)
+GUID=$(dd if=/dev/random bs=12 count=1 2>/dev/null | od -An -tx1 | tr -d ' \t\n')
+TRACE_ID="1-$HEX_TIME-$GUID"
+END_TIME=$(($START_TIME+3))
+DOC=$(cat <<EOF
+{"trace_id": "$TRACE_ID", "id": "6226467e3f845502", "start_time": $START_TIME.37518, "end_time": $END_TIME.4042, "name": "test.elasticbeanstalk.com"}
+EOF
+)
+export AWS_ACCESS_KEY_ID=7373737373
+export AWS_SECRET_ACCESS_KEY=uuigsjdfgiusdf
+aws --endpoint-url http://localhost:3000 --no-verify-ssl xray put-trace-segments --trace-segment-documents "$DOC"
+```
---
+++ main.go
@@ -0,0 +1,159 @@
+package main
+
+import (
+	"encoding/json"
+	"os"
+	"sync"
+
+	"net/http"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+var logger *zap.Logger
+var statusManager = NewStatusManager()
+var StatusOK = "OK"
+var StatusThrottled = "Throttled"
+
+type StatusManager struct {
+	mutex  sync.RWMutex
+	status string
+}
+
+func NewStatusManager() *StatusManager {
+	return &StatusManager{
+		status: StatusOK,
+	}
+}
+
+func (sm *StatusManager) GetStatus() string {
+	sm.mutex.RLock()
+	defer sm.mutex.RUnlock()
+	return sm.status
+}
+
+func (sm *StatusManager) SetThrottled(status string) {
+	sm.mutex.Lock()
+	defer sm.mutex.Unlock()
+	sm.status = StatusThrottled
+	logger.Info("status changed", zap.String("status", sm.status))
+}
+
+func (sm *StatusManager) SetOK() {
+	sm.mutex.Lock()
+	defer sm.mutex.Unlock()
+	sm.status = StatusOK
+	logger.Info("status changed", zap.String("status", sm.status))
+}
+
+// Response represents the structure of our JSON response
+type Response struct {
+	UnprocessedTraceSegments []TraceSegment `json:"UnprocessedTraceSegments"`
+}
+
+// TraceSegment represents each segment in the response
+type TraceSegment struct {
+	Id        string `json:"Id"`
+	ErrorCode string `json:"ErrorCode"`
+	Message   string `json:"Message"`
+}
+
+// ThrottledException represents the structure of the error response
+type ThrottledException struct {
+	Message string `json:"message"`
+	Type    string `json:"__type"`
+}
+
+func handleSetOK(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	statusManager.SetOK()
+	w.WriteHeader(http.StatusOK)
+}
+
+func handleSetThrottled(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	statusManager.SetThrottled(StatusThrottled)
+	w.WriteHeader(http.StatusOK)
+}
+
+func handleTraceSegments(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	status := statusManager.GetStatus()
+
+	if status == StatusThrottled {
+		response := ThrottledException{
+			Message: "Rate exceeded",
+			Type:    "ThrottlingException",
+		}
+
+		logger.Info("Received request",
+			zap.String("method", r.Method),
+			zap.String("path", r.URL.Path),
+			zap.String("status_code", "429"),
+			zap.String("message", "Rate Exceeded"),
+			zap.String("user_agent", r.UserAgent()),
+		)
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusTooManyRequests)
+		json.NewEncoder(w).Encode(response)
+		return
+	} else {
+		response := Response{
+			UnprocessedTraceSegments: []TraceSegment{
+				{
+					Id:        "",
+					ErrorCode: "",
+					Message:   "",
+				},
+			},
+		}
+
+		logger.Info("Received request",
+			zap.String("method", r.Method),
+			zap.String("path", r.URL.Path),
+			zap.String("status_code", "200"),
+			zap.String("message", "ok"),
+			zap.String("user_agent", r.UserAgent()),
+		)
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(response)
+	}
+}
+
+func main() {
+	// Register handler for /TraceSegments path
+	http.HandleFunc("/TraceSegments", handleTraceSegments)
+	http.HandleFunc("/SetOK", handleSetOK)
+	http.HandleFunc("/SetThrottled", handleSetThrottled)
+
+	config := zap.NewProductionEncoderConfig()
+	config.EncodeTime = zapcore.ISO8601TimeEncoder
+	encoder := zapcore.NewJSONEncoder(config)
+	core := zapcore.NewTee(
+		zapcore.NewCore(encoder, zapcore.AddSync(zapcore.AddSync(zapcore.Lock(os.Stdout))), zapcore.DebugLevel),
+	)
+
+	logger = zap.New(core)
+
+	// Start server on port 8080
+	logger.Info("Starting server on :8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		logger.Fatal("Server failed to start: %v", zap.Error(err))
+	}
+}
